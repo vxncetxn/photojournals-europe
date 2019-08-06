3677c7186eea4aa96c4e917a8d6b88a7f41c2df9
Vance <thevancetan@gmail.com>
2019-08-07 03:10:55 +0800

    responsive design v4, made mobile photos grid container

--- src/components/Content.js
+++ src/components/Content.js
@@ -19,7 +19,7 @@ const Content = styled.div`
   }
 
   @media (max-width: 520px) {
-    padding: 5rem 3rem;
+    padding: 5rem 2rem;
   }
 `;
 
@@ -55,13 +55,13 @@ const ContentTitle = styled.h2`
       1px 1px 0px var(--color-white);
   }
 
-  @media (max-width: 520px) {
-    text-shadow: -0.5px -0.5px 0px var(--color-white),
-      0px -0.5px 0px var(--color-white), 0.5px -0.5px 0px var(--color-white),
-      -0.5px 0px 0px var(--color-white), 0.5px 0px 0px var(--color-white),
-      -0.5px 0.5px 0px var(--color-white), 0px 0.5px 0px var(--color-white),
-      0.5px 0.5px 0px var(--color-white);
-  }
+  // @media (max-width: 520px) {
+  //   text-shadow: -0.5px -0.5px 0px var(--color-white),
+  //     0px -0.5px 0px var(--color-white), 0.5px -0.5px 0px var(--color-white),
+  //     -0.5px 0px 0px var(--color-white), 0.5px 0px 0px var(--color-white),
+  //     -0.5px 0.5px 0px var(--color-white), 0px 0.5px 0px var(--color-white),
+  //     0.5px 0.5px 0px var(--color-white);
+  // }
 `;
 
 const ContentBody = styled.div`
@@ -81,24 +81,25 @@ const ContentBody = styled.div`
 
 const Writeup = styled.div`
   font-family: var(--font-secondary), sans-serif;
-  font-size: 3.6rem;
+  // font-size: 3.6rem;
+  font-size: calc(2rem + 1vw);
   font-weight: 200;
   line-height: 1.6;
   width: 30vw;
   flex: 0 0 auto;
   // border: 2px solid red;
-  margin-right: 60px;
+  margin-right: 50px;
 
   @media (max-width: 800px), (orientation: portrait) {
     width: auto;
     margin-right: 0px;
     margin-bottom: 50px;
-    font-size: calc(2.5rem + 1vw);
+    // font-size: calc(2.5rem + 1vw);
   }
 `;
 
-let Photos;
-Photos = styled.ul`
+let DefaultPhotos;
+DefaultPhotos = styled.ul`
   //   width: 100vw;
   flex: 0 0 auto;
   // border: 2px solid blue;
@@ -119,10 +120,57 @@ Photos = styled.ul`
     // grid-auto-columns: calc(100px + 2vw);
   }
 
-  & ${Photos}:nth-child(1),
-  & ${Photos}:nth-child(4n) {
+  & ${DefaultPhotos}:nth-child(1),
+  & ${DefaultPhotos}:nth-child(4n) {
     grid-column: span 2;
     grid-row: 1 / 3;
+  }
+`;
+
+// const MobilePhotos = styled.ul`
+//   display: grid;
+//   grid-template-rows: calc(100vh - 10rem);
+//   grid-auto-columns: 100%;
+//   gap: 2px;
+//   grid-auto-flow: column;
+//   overflow: scroll;
+//   -webkit-overflow-scrolling: touch;
+//   scroll-snap-type: x mandatory;
+//   -webkit-scroll-snap-type: mandatory;
+//   -webkit-scroll-snap-points-x: repeat(100%);
+//   scroll-snap-points-x: repeat(100%);
+//   background-color: #9485e0;
+//   // border: 2px solid red;
+
+//   & > .gatsby-image-wrapper {
+//     scroll-snap-align: start;
+//   }
+// `;
+
+// const MobilePhotosTwo = styled.ul`
+//   & > .gatsby-image-wrapper {
+//     width: 100%;
+//     margin-top: 20px;
+//   }
+// `;
+
+const MobilePhotos = styled.ul`
+  display: grid;
+  height: calc(100vh - 10rem);
+  grid-template-rows: 1fr 1fr;
+  grid-auto-columns: 90%;
+  gap: 2px;
+  grid-auto-flow: column;
+  overflow: scroll;
+  -webkit-overflow-scrolling: touch;
+
+  & > .portrait {
+    grid-row: 1 / 3;
+  }
+
+  & > .gatsby-image-wrapper {
+    // border: 1px solid red;
+    background-color: #9485e0;
   }
 `;
 
@@ -173,11 +221,65 @@ const ContentComp = ({ color, loc, locPhotos }) => {
   const [clickedPhotoIdx, setClickedPhotoIdx] = useState(NaN);
   const [scrollLoc, setScrollLoc] = useState(0);
 
-  const [isOnMobile, setIsOnMobile] = useState(window.innerWidth < 801);
+  const [isOnMobile, setIsOnMobile] = useState(false);
 
   const scrollerRef = useRef();
 
+  const portraitLocPhotos = [];
+  const landscapeLocPhotos = [];
+  const pairedLandscapeLocPhotos = [];
+  const mobileLocPhotos = [];
+  locPhotos.forEach(photo => {
+    if (photo.fluid.aspectRatio < 1) {
+      portraitLocPhotos.push(photo);
+    } else {
+      landscapeLocPhotos.push(photo);
+    }
+  });
+  if (landscapeLocPhotos.length % 2) {
+    for (let n = 0; n < Math.floor(landscapeLocPhotos.length / 2); n++) {
+      const group = [landscapeLocPhotos[n * 2], landscapeLocPhotos[n * 2 + 1]];
+      pairedLandscapeLocPhotos.push(group);
+    }
+    pairedLandscapeLocPhotos.push([
+      landscapeLocPhotos[landscapeLocPhotos.length - 1],
+      null
+    ]);
+  } else {
+    for (let n = 0; n < landscapeLocPhotos.length / 2; n++) {
+      const group = [landscapeLocPhotos[n * 2], landscapeLocPhotos[n * 2 + 1]];
+      pairedLandscapeLocPhotos.push(group);
+    }
+  }
+
+  const minLength = Math.min(
+    portraitLocPhotos.length,
+    pairedLandscapeLocPhotos.length
+  );
+
+  for (let n = 0; n < minLength; n++) {
+    mobileLocPhotos.push(portraitLocPhotos[n]);
+    pairedLandscapeLocPhotos[n].forEach(item => {
+      mobileLocPhotos.push(item);
+    });
+  }
+
+  if (portraitLocPhotos.length > pairedLandscapeLocPhotos.length) {
+    for (let n = minLength; n < portraitLocPhotos.length; n++) {
+      mobileLocPhotos.push(portraitLocPhotos[n]);
+    }
+  } else {
+    for (let n = minLength; n < pairedLandscapeLocPhotos.length; n++) {
+      pairedLandscapeLocPhotos[n].forEach(item => {
+        if (item) {
+          mobileLocPhotos.push(item);
+        }
+      });
+    }
+  }
+
   useEffect(() => {
+    setIsOnMobile(window.innerWidth < 801);
     window.addEventListener("resize", () => {
       if (window.innerWidth < 801) {
         setIsOnMobile(true);
@@ -231,7 +333,7 @@ const ContentComp = ({ color, loc, locPhotos }) => {
       });
 
       document
-        .querySelectorAll(`.content-photos-${loc} .gatsby-image-wrapper`)
+        .querySelectorAll(`.content-photos-${loc} .photos-hover-active`)
         .forEach((photo, idx) => {
           photo.addEventListener("mouseenter", () => {
             photo.childNodes[2].style.opacity = "0.6";
@@ -292,20 +394,45 @@ const ContentComp = ({ color, loc, locPhotos }) => {
           <ContentBody ref={isOnMobile ? null : scrollerRef}>
             <Writeup>
               <span style={{ color: "var(--color-white)" }}>
-                Istanbul is a major city in Turkey
+                Following the footsteps of many a hundred tiny little dwarves
+                here all day long
               </span>{" "}
               <span style={{ color: "var(--color-gray)" }}>
-                that straddles Europe and Asia across the Bosphorus Strait.
+                brought me to charming and colourful streets and buildings,
+                tantalising pierogis and the most authentic locals.
               </span>
             </Writeup>
-            <Photos
-              ref={isOnMobile ? scrollerRef : null}
-              className={`content-photos-${loc}`}
-            >
-              {locPhotos.map((photo, idx) => {
-                return <StyledImg key={idx} fluid={photo.fluid} />;
-              })}
-            </Photos>
+            {isOnMobile ? (
+              <MobilePhotos
+                ref={scrollerRef}
+                className={`content-photos-${loc}`}
+              >
+                {mobileLocPhotos.map((photo, idx) => {
+                  return (
+                    <Img
+                      key={idx}
+                      fluid={photo.fluid}
+                      className={
+                        photo.fluid.aspectRatio < 1 ? "portrait" : null
+                      }
+                      imgStyle={{ objectFit: "cover" }}
+                    />
+                  );
+                })}
+              </MobilePhotos>
+            ) : (
+              <DefaultPhotos className={`content-photos-${loc}`}>
+                {locPhotos.map((photo, idx) => {
+                  return (
+                    <StyledImg
+                      key={idx}
+                      fluid={photo.fluid}
+                      className="photos-hover-active"
+                    />
+                  );
+                })}
+              </DefaultPhotos>
+            )}
           </ContentBody>
         </>
       )}
